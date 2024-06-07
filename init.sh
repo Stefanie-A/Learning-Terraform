36284450181632d83e99486e397377eab8395bcc
Stefanie-A <albertstephanie630@gmail.com>
2024-06-07 14:42:18 -0700

    feat: security group

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Welcome!" > index.html
+nohup busybox httpd -f -p 8080 &
 sudo apt update
 sudo apt upgrade
-echo "Welcome!"
--- main.tf
+++ main.tf
@@ -1,14 +1,24 @@
 provider "aws"{
-
-    region = "us-east-1b"
-
-    tags ={
-        name = "fox"
-    }
-
-    user-data = file(init.sh)
+    region = "us-east-1"
+    
 }
-resources "aws_instance" "web_server"{
+resource "aws_security_group" "instance"{
+    name = "fox"
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+resource "aws_instance" "web_server"{
     ami = "ami-0fb653ca2d3203ac1"
-    instance-type = "t2.micro"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+    tags = {
+        Name = "fox"
+    }
+    user_data = file(init.sh)
+    user_data_replace_on_change = true
+
 }
